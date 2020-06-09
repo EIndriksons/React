1820deac8d27f315f963ec09f0dbed836555848a
EIndriksons <edgars@indriksons.lv>
2020-06-10 00:36:55 +0300

    .bind(this, "") (old method)

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ class App extends Component {
         ]
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         this.setState({
             persons: [
-                { name: "Maximilian", age: 28 },
+                { name: newName, age: 28 },
                 { name: "Manu", age: 29 },
                 { name: "Stephanie", age: 27 },
             ]
@@ -26,14 +26,14 @@ class App extends Component {
         <div className="App">
             <h1>Hi, I'm a React App</h1>
             <p>This is really working!</p>
-            <button onClick={this.switchNameHandler}>Switch Name</button>
+            <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
             <Person
                 name={this.state.persons[0].name}
                 age={this.state.persons[0].age} />
             <Person
                 name={this.state.persons[1].name}
                 age={this.state.persons[1].age}
-                click={this.switchNameHandler} >My Hobbies: Racing</Person>
+                click={this.switchNameHandler.bind(this, 'Max!')} >My Hobbies: Racing</Person>
             <Person
                 name={this.state.persons[2].name}
                 age={this.state.persons[2].age} />

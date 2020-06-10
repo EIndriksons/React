6f2c32168b410ef9203a0a07a8fd56db52bd049d
EIndriksons <edgars@indriksons.lv>
2020-06-10 17:58:36 +0300

    Added state and usernameChangedHandler

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import UserOutput from './UserOutput/UserOutput';
 import UserInput from './UserInput/UserInput';
 
 class App extends Component {
+    state = {
+        username: 'Edgars'
+    }
+
+    usernameChangeHandler = (event) => {
+        this.setState({
+            username: event.target.value
+        });
+    }
+
     render() {
         return (
         <div className="App">
             <UserInput />
-            <UserOutput />
-            <UserOutput />
+            <UserOutput username={this.state.username} />
         </div>
         );
     }

44890fc006a3f0345904bf9aede7b116863b60eb
EIndriksons <edgars@indriksons.lv>
2020-06-10 21:19:21 +0300

    Assignment 1 init

--- src/App.js
+++ src/App.js
@@ -1,86 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
 
 class App extends Component {
-    state = {
-        persons: [
-            { id: 0, name: "Max", age: 28 },
-            { id: 1, name: "Manu", age: 29 },
-            { id: 2, name: "Stephanie", age: 26 },
-        ],
-        otherState: 'some other value',
-        showPersons: false,
-    }
-
-    nameChangedHandler = (event, id) => {
-        const personIndex = this.state.persons.findIndex(p => {
-            return p.id === id;
-        });
-
-        const person = {
-            ...this.state.persons[personIndex] // use this to generate new array instead of getting a pointer
-        }
-
-        // or use this instead of above
-        // const person = Object.assign({}, this.state.persons[personIndex])
-
-        person.name = event.target.value;
-
-        const persons = [...this.state.persons];
-        persons[personIndex] = person;
-
-        this.setState({persons: persons})
-    }
-
-    deletePersonHandler = (personIndex) => {
-        const persons = [...this.state.persons]; // use this to generate new array instead of getting a pointer
-                                                 // best practice is to generate actual copy, change it and then update state
-        persons.splice(personIndex, 1);
-        this.setState({persons: persons})
-    }
-
-    togglePersonsHandler = () => {
-        const doesShow = this.state.showPersons;
-        this.setState({showPersons: !doesShow})
-    }
-
     render() {
-        const style = {
-            backgroundColor: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        };
-
-        let persons = null;
-        if (this.state.showPersons) {
-            persons = (
-                <div>
-                    {this.state.persons.map((person, index) => {
-                        return <Person
-                            click={() => this.deletePersonHandler(index)}
-                            name={person.name}
-                            age={person.age}
-                            key={person.id}
-                            changed={(event) => this.nameChangedHandler(event, person.id)} />
-                    })}
-                </div>
-            );
-        }
-
         return (
         <div className="App">
-            <h1>Hi, I'm a React App</h1>
-            <p>This is really working!</p>
-            <button
-                style={style}
-                onClick={this.togglePersonsHandler}>Switch Name</button>
-            {persons}
+            
         </div>
         );
     }
 }
 
 export default App;
+
+{/* <ol>
+<li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+<li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+<li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+<li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+<li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+<li>When you click a CharComponent, it should be removed from the entered text.</li>
+</ol>
+<p>Hint: Keep in mind that JavaScript strings are basically arrays!</p> */}

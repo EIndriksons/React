4851241269d74780bbc2c459e402771e6c5dd238
EIndriksons <edgars@indriksons.lv>
2020-07-07 18:33:00 +0300

    CSS Modules 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -63,24 +63,25 @@ class App extends Component {
             );
         }
 
-        let classes = [];
+        let assignedClasses = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red');
+            assignedClasses.push(classes.red);
         }
 
         if (this.state.persons.length <= 1) {
-            classes.push('bold');
+            assignedClasses.push(classes.bold);
         }
 
         return (
-        <div className="App">
+        <div className={classes.App}>
             <h1>Hi, I'm a React App</h1>
-            <p className={classes.join(' ')}>This is really working!</p>
-            <StyledButton
+            <p className={assignedClasses.join(' ')}>This is really working!</p>
+            <button
+                className={classes.Button}
                 alt={this.state.showPersons}
                 onClick={this.togglePersonsHandler}
             >Switch Name
-            </StyledButton>
+            </button>
             {persons}
         </div>
         );

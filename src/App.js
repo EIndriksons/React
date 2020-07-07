e0a9dc0b289970d816882fd756b57e10d9279f4d
EIndriksons <edgars@indriksons.lv>
2020-07-07 19:29:23 +0300

    Error Boundaries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
     state = {
@@ -54,12 +55,13 @@ class App extends Component {
             persons = (
                 <div>
                     {this.state.persons.map((person, index) => {
-                        return <Person
+                        return <ErrorBoundary key={person.id}>
+                            <Person
                             click={() => this.deletePersonHandler(index)}
                             name={person.name}
                             age={person.age}
-                            key={person.id}
                             changed={(event) => this.nameChangedHandler(event, person.id)} />
+                        </ErrorBoundary>
                     })}
                 </div>
             );
@@ -82,7 +84,6 @@ class App extends Component {
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
                 className={btnClass}
-                alt={this.state.showPersons}
                 onClick={this.togglePersonsHandler}
             >Switch Name
             </button>

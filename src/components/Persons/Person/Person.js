6e064c26cb3c432adddec5ef9b37000ca92c5ed1
EIndriksons <edgars@indriksons.lv>
2020-07-10 16:22:48 +0300

    React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,11 +6,11 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <Aux>
+            <React.Fragment>
                 <p key="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p key="2">{this.props.children}</p>
                 <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
-            </Aux>
+            </React.Fragment>
         );
     }
 };

bcf9e5549bd788fbc107a8fdaf482b3347fb836b
EIndriksons <edgars@indriksons.lv>
2020-07-14 20:58:01 +0300

    Passing props in wrapped functions

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// {...} - spread operator which pulls out all props key value pairs and distributes them
+// from { name: 'Maximilian', age: 28 }
+// to name = 'Maximilian' age = 28
+
 const withClass = (WrappedComponent, className) => {
     return props => (
         <div className={className}>
-            <WrappedComponent />
+            <WrappedComponent {...props} />
         </div>
     );
 };

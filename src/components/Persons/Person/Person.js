9c9a572cbb490277d809de3b714307f8e0268666
EIndriksons <edgars@indriksons.lv>
2020-07-14 21:21:29 +0300

    Using references

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,13 +6,30 @@ import Aux from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
+    constructor(props) {
+        super();
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
             <React.Fragment>
                 <p key="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p key="2">{this.props.children}</p>
-                <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
+                <input
+                    key="3"
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
             </React.Fragment>
         );
     }

99b69715d747170f5bade236ee5677fb9dbd5d7d
EIndriksons <edgars@indriksons.lv>
2020-07-14 21:03:27 +0300

    Setting State correctly - wrong way

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,6 +20,7 @@ class App extends Component {
         otherState: 'some other value',
         showPersons: false,
         showCockpit: true,
+        changeCounter: 0,
     }
 
     static getDerivedStateFromProps(props, state) {
@@ -61,7 +62,10 @@ class App extends Component {
         const persons = [...this.state.persons];
         persons[personIndex] = person;
 
-        this.setState({persons: persons})
+        this.setState({
+            persons: persons,
+            changeCounter: this.state.changeCounter + 1
+        })
     }
 
     deletePersonHandler = (personIndex) => {

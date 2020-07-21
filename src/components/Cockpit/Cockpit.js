97fcdc5f07d763cab98dac37967c5a0b2bb17613
EIndriksons <edgars@indriksons.lv>
2020-07-21 21:43:07 +0300

    Context API 2

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
+import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
 
@@ -47,7 +48,9 @@ const cockpit = (props) => {
                 className={btnClass}
                 onClick={props.clicked} >
                 Switch Name</button>
-            <button onClick={props.login}>Log in</button>
+            <AuthContext.Consumer>
+                {(context) => <button onClick={context.login}>Log in</button>}
+            </AuthContext.Consumer>
         </div>
     );
 };

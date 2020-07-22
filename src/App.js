d351d23c40a0eef3370515a8f8af23ac2bdd2ab5
EIndriksons <edgars@indriksons.lv>
2020-07-22 12:29:40 +0300

    Burger Builder component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-render() {
-    return (
-    <React.Fragment>
-        <Layout>
-            <p>Test</p>
-        </Layout>
-    </React.Fragment>
-    );
-}
+    render() {
+        return (
+        <React.Fragment>
+            <Layout>
+                <BurgerBuilder />
+            </Layout>
+        </React.Fragment>
+        );
+    }
 }
 
 export default App;
